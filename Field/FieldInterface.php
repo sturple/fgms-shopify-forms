40b070536bee34391b7b55fd3edb4544b75b115a
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-12-07 18:46:42 -0800

    FieldInterface
    Actually added methods to this interface.

--- Field/FieldInterface.php
+++ Field/FieldInterface.php
@@ -2,7 +2,51 @@
 
 namespace Fgms\EmailInquiriesBundle\Field;
 
+/**
+ * An interface for domain objects which represent
+ * a Field entity.
+ */
 interface FieldInterface
 {
-    
+    /**
+     * Retrieves the wrapped Field entity.
+     *
+     * @return Field
+     */
+    public function getField();
+
+    /**
+     * Processes the submission of this field.
+     *
+     * @param ValueWrapper $obj
+     *  All values submitted as part of the current request.
+     * @param Submission $submission
+     *  The Submission entity associated with this submission.
+     */
+    public function submit(\Fgms\EmailInquiriesBundle\Utility\ValueWrapper $obj, \Fgms\EmailInquiriesBundle\Entity\Submission $submission);
+
+    /**
+     * Generates sections for the email from submitted
+     * data for this field.
+     *
+     * @param Submission $submission
+     *  The Submission entity associated with this submission.
+     *
+     * @return array
+     *  An array of strings each of which is the raw body
+     *  of a section.
+     */
+    public function render(\Fgms\EmailInquiriesBundle\Entity\Submission $submission);
+
+    /**
+     * Allows a field an opportunity to adjust the message
+     * before it is sent.
+     *
+     * @param Swift_Message $message
+     *  An object representing the email message which is about
+     *  to be sent.
+     * @param Submission $submission
+     *  The Submission entity associated with this submission.
+     */
+    public function filterMessage(\Swift_Message $message, \Fgms\EmailInquiriesBundle\Entity\Submission $submission);
 }
