bb09fbb7732de8dc77e3481877b638d8b5d88a6b
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-12-08 01:35:05 -0800

    BasicWrapper - Tabs => Spaces

--- Utility/BasicWrapper.php
+++ Utility/BasicWrapper.php
@@ -5,12 +5,12 @@ namespace Fgms\EmailInquiriesBundle\Utility;
 trait BasicWrapper
 {
     public function raiseTypeMismatch($key, $expected, $actual)
-	{
-		throw new \LogicException('Type mismatch');
-	}
+    {
+        throw new \LogicException('Type mismatch');
+    }
 
-	public function raiseMissing($key)
-	{
-		throw new \LogicException('Missing');
-	}
+    public function raiseMissing($key)
+    {
+        throw new \LogicException('Missing');
+    }
 }
